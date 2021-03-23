d6bdc54a4b1398338d068c9114cd1a6f143db5e4
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-23 10:18:36 -0600

    Agregar archivos de proyecto.

---
+++ Hilos_3/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Hilos_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnporceso1 = new System.Windows.Forms.Button();
+            this.btnporceso2 = new System.Windows.Forms.Button();
+            this.btnporceso3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnporceso1
+            // 
+            this.btnporceso1.Font = new System.Drawing.Font("Muthiara -Demo Version-", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnporceso1.Location = new System.Drawing.Point(120, 309);
+            this.btnporceso1.Name = "btnporceso1";
+            this.btnporceso1.Size = new System.Drawing.Size(118, 55);
+            this.btnporceso1.TabIndex = 0;
+            this.btnporceso1.Text = "Proceso 1";
+            this.btnporceso1.UseVisualStyleBackColor = true;
+            this.btnporceso1.Click += new System.EventHandler(this.btnporceso1_Click);
+            // 
+            // btnporceso2
+            // 
+            this.btnporceso2.Font = new System.Drawing.Font("Muthiara -Demo Version-", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnporceso2.Location = new System.Drawing.Point(265, 309);
+            this.btnporceso2.Name = "btnporceso2";
+            this.btnporceso2.Size = new System.Drawing.Size(118, 55);
+            this.btnporceso2.TabIndex = 1;
+            this.btnporceso2.Text = "Proceso 2";
+            this.btnporceso2.UseVisualStyleBackColor = true;
+            this.btnporceso2.Click += new System.EventHandler(this.btnporceso2_Click);
+            // 
+            // btnporceso3
+            // 
+            this.btnporceso3.Font = new System.Drawing.Font("Muthiara -Demo Version-", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnporceso3.Location = new System.Drawing.Point(437, 309);
+            this.btnporceso3.Name = "btnporceso3";
+            this.btnporceso3.Size = new System.Drawing.Size(118, 55);
+            this.btnporceso3.TabIndex = 2;
+            this.btnporceso3.Text = "Proceso 3";
+            this.btnporceso3.UseVisualStyleBackColor = true;
+            this.btnporceso3.Click += new System.EventHandler(this.btnporceso3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnporceso3);
+            this.Controls.Add(this.btnporceso2);
+            this.Controls.Add(this.btnporceso1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnporceso1;
+        private System.Windows.Forms.Button btnporceso2;
+        private System.Windows.Forms.Button btnporceso3;
+    }
+}
+
---
+++ Hilos_3/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Hilos_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Thread h1, h2, h3;
+        Random rd = new Random();
+
+        private void btnporceso1_Click(object sender, EventArgs e)
+        {
+            h1 = new Thread(new ThreadStart(Proceso1));
+            h1.Start();
+           // h1.Join();
+        }
+
+
+        private void btnporceso2_Click(object sender, EventArgs e)
+        {
+            h2 = new Thread(new ThreadStart(Proceso2));
+            h2.Start();
+            //h2.Join();
+        }
+
+
+        private void btnporceso3_Click(object sender, EventArgs e)
+        {
+            h3 = new Thread(new ThreadStart(Proceso3));
+            h3.Start();
+            //h3.Join();
+
+        }
+
+
+        public void Proceso1()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                try
+                {
+                    this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(rd.Next(0, this.Width), (rd.Next(0, this.Height)), 20, 20));
+                }
+                catch (Exception )
+                {
+                    MessageBox.Show("Se interrumpio el proceso");
+                }
+
+                //this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red,4),new Rectangle(rd.Next(0,this.Width), (rd.Next(0, this.Height)), 20, 20 ));
+
+            }
+
+            Thread.Sleep(1000);
+            h1.Join();
+
+        }
+
+        public void Proceso2()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                try
+                {
+                    this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 4), new Rectangle(rd.Next(0, this.Width), (rd.Next(0, this.Height)), 20, 20));
+                }
+                catch (Exception )
+                {
+                    MessageBox.Show("Se interrumpio el proceso");
+                }
+
+
+                //this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 4), new Rectangle(rd.Next(0, this.Width), (rd.Next(0, this.Height)), 20, 20));
+
+            }
+
+            Thread.Sleep(1000);
+            h2.Join();
+        }
+
+        public void Proceso3()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                try
+                {
+                    this.CreateGraphics().DrawRectangle(new Pen(Brushes.Violet, 4), new Rectangle(rd.Next(0, this.Width), (rd.Next(0, this.Height)), 20, 20));
+                }
+                catch (Exception )
+                {
+                    MessageBox.Show("Se interrumpio el proceso");
+                }
+
+                //this.CreateGraphics().DrawRectangle(new Pen(Brushes.Violet, 4), new Rectangle(rd.Next(0, this.Width), (rd.Next(0, this.Height)), 20, 20));
+
+            }
+
+            Thread.Sleep(1000);
+            h3.Join();
+        }
+         
+
+    }
+}
